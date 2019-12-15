6e20e128f3d1e99c15c8a24a78281dc21aa2d8d3
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-12-15 13:15:23 -0500

    Go Implementation

---
+++ auth-server-go/server.go
@@ -0,0 +1,10 @@
+package (
+	"fmt"
+	"net/http"
+)
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
